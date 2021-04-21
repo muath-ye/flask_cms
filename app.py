93f88342230c4fbeb592dae4252452d2a2fec0c7
Muathye <muath.ye@gmail.com>
2021-04-22 01:59:37 +0300

    update and fixes

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 # -*- coding:utf8 -*-
-from flask import Flask, request, render_template, redirect, url_for
-import manage_db 
+from flask import Flask, request, render_template, redirect, url_for, session
+import manage_db
+from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "\xee\x9dA\x81\x19\x17\xdd\x04\xae9\xc1\x1a-\xf2\xf8\xda\x9a\x99u\x90\x96]\xbaT"
+
+def login_required(function):
+    @wraps(function) 
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            return function(*args, **kwargs)
+        else:
+            return redirect(url_for('home'))
+    return wrapper
 
 # Home Page 
 @app.route("/") 
@@ -30,6 +41,25 @@ def post(post_id):
 def delete(post_id): 
     manage_db.delete(post_id)
     return redirect(url_for('home'))
+
+# Login Route
+@app.route("/login", methods=['GET', 'POST']) 
+def login(): 
+    if request.method == 'POST':
+        username = request.form['username'] 
+        password = request.form['password'] 
+	
+        if username == "admin" and password == "password": 
+            session['logged_in'] = True # create new session with name 'logged_in'
+            return redirect(url_for('home'))
+        else: 
+            return redirect(url_for('home'))
+	
+# Logout Route 
+@app.route("/logout") 
+def logout(): 
+	session.pop('logged_in', None) 
+	return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=true)
