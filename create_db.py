aadc8869c28835ceadcf7239981837ecbb90a0d6
Muathye <muath.ye@gmail.com>
2021-04-22 00:02:53 +0300

    fix create_db syntax

--- create_db.py
+++ create_db.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*- 
-
-import sqlite3 
+import sqlite3
 
 # الاتّصال بقاعدة البيانات 
-db = sqlite3.connect('database.db') 
+# You can create a new database by changing the name within the quotes
+db = sqlite3.connect('database.db')
 
 # إنشاء مُؤشّر في قاعدة البيانات لنتمكّن من تنفيذ استعلامات 
-SQL cursor = db.cursor() 
+# The database will be saved in the location where your 'py' file is saved
+cursor = db.cursor()
 
 # إنشاء الجدول 
-cursor.execute(""" CREATE TABLE posts( id INTEGER PRIMARY KEY, title CHAR(200), content TEXT )""") 
+# Create table - CLIENTS
+cursor.execute('''CREATE TABLE posts
+             (
+                 [id] INTEGER PRIMARY KEY,
+                 [title] CHAR(200),
+                 [content] TEXT
+            )
+        ''')
+
 
 # إدخال القيم إلى الجدول 
 cursor.execute('''INSERT INTO posts(title, content) VALUES(?,?)''', (u'عنوان المقال الأول', u'محتوى المقال الأول'))
 cursor.execute('''INSERT INTO posts(title, content) VALUES (?,?)''', (u'عنوان المقال الثّاني', u'مُحتوى المقال الثّاني'))
 
-# تطبيق التغييرات
+
 db.commit()
